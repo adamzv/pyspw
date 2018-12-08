bb2403f5a1cb71bb055b7f93d63c948105602f5a
Adam Zverka <adamzverka@hotmail.com>
2018-12-08 21:32:30 +0100

    Ošetrenie

--- pyServletPrintWriter.py
+++ pyServletPrintWriter.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 
 
 def main():
@@ -8,27 +9,34 @@ def main():
     parser.add_argument("-w", dest="zapis")
 
     args = parser.parse_args()
-    # ošetriť
 
-    zapis = None
-
-    subor = open(args.subor[0], encoding="utf8")
-
-    if args.zapis is not None:
+    try:
+        subor = open(args.subor[0], "r", encoding="utf8")
         zapis = open(args.zapis, "a", encoding="utf8")
-
-    for line in enumerate(subor):
-        line = line[1].rstrip()
-        line = line.replace("\"", "\\\"")
-        line = f"out.println(\"{line}\");"
-        if zapis is not None:
-            zapis.write(line + "\n")
+    # Ak sa pri vstupe nezadá argument -w, tak hodnota atribútu args.zapis bude None
+    except TypeError:
+        zapis = None
+    # Ak sa nenájde súbor z ktorého chceme čítať
+    except FileNotFoundError:
+        print("Súbor na čítanie sa nenašiel.")
+        subor = None
+    finally:
+        # Ak sa súbor na čítanie nenašiel, ukonči program
+        if subor is None:
+            sys.exit(1)
         else:
-            print(line)
-    subor.close()
+            for line in enumerate(subor):
+                line = line[1].rstrip()
+                line = line.replace("\"", "\\\"")
+                line = f"out.println(\"{line}\");"
+                if zapis is not None:
+                    zapis.write(line + "\n")
+                else:
+                    print(line)
+            subor.close()
 
-    if zapis is not None:
-        zapis.close()
+            if zapis is not None:
+                zapis.close()
 
 
 if __name__ == "__main__":
