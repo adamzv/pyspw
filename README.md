41290b6da23595b0f6cdfece5b063c9831e1a0bb
Adam Zverka <adamzverka@hotmail.com>
2018-12-09 11:30:23 +0100

    Vytvorenie metódy spracuj

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ out.println("    <meta charset=\"UTF-8\">");
 out.println("    <title>Title</title>");
 out.println("</head>");
 out.println("<body>");
-out.println("");
+out.println();
 out.println("</body>");
 out.println("</html>");
 ```
--- pyServletPrintWriter.py
+++ pyServletPrintWriter.py
@@ -1,49 +1,59 @@
 import argparse
 import sys
+from pathlib import Path
 
 
-def parse_args(args):
+def create_parser():
     parser = argparse.ArgumentParser(description="Spracuje html súbor do podoby využiteľnej v servletoch.\n"
                                                  "napr. out.println(\"<h1>Java PrintWriter</h1>\");")
     parser.add_argument("subor", metavar="subor", nargs=1, help="súbor, ktorého dáta sa majú spracovať")
-    parser.add_argument("-w", dest="zapis")
+    parser.add_argument("-w", dest="zapis", required=False, help="voliteľný súbor, do ktorého sa majú zapísať spracované dáta")
 
-    return parser.parse_args(args)
+    return parser
 
 
-def main():
-    args = parse_args(sys.argv[1:])
+def spracuj(vstupny_subor, vystupny_subor=None):
+    subor_na_citanie = Path(vstupny_subor)
 
-    try:
-        subor = open(args.subor[0], "r", encoding="utf8")
-        zapis = open(args.zapis, "a", encoding="utf8")
-    # Ak sa pri vstupe nezadá argument -w, tak hodnota atribútu args.zapis bude None
-    except TypeError:
-        zapis = None
-    # Ak sa nenájde súbor z ktorého chceme čítať
-    except FileNotFoundError:
-        subor = None
-        print("Súbor na čítanie sa nenašiel.")
-    finally:
-        # Ak sa súbor na čítanie nenašiel, ukonči program
-        if subor is None:
-            sys.exit(1)
-        else:
-            for line in enumerate(subor):
+    # ak sa vstupný súbor nenašiel, ukonči spracovanie
+    if not subor_na_citanie.is_file():
+        print("Súbor na čítanie sa nenašiel")
+        return False
+    else:
+        try:
+            vstup = open(vstupny_subor, "r", encoding="utf8")
+            vystup = open(vystupny_subor, "a", encoding="utf8")
+
+        # Ak sa pri vstupe nezadá argument -w, tak hodnota atribútu args.zapis bude None
+        except TypeError:
+            vystup = None
+        finally:
+            for line in enumerate(vstup):
                 line = line[1].rstrip()
                 if line == "":
                     line = "out.println();"
                 else:
                     line = line.replace("\"", "\\\"")
                     line = f"out.println(\"{line}\");"
-                if zapis is not None:
-                    zapis.write(line + "\n")
+                if vystup is not None:
+                    vystup.write(line + "\n")
                 else:
                     print(line)
 
-            subor.close()
-            if zapis is not None:
-                zapis.close()
+            vstup.close()
+            if vystup is not None:
+                vystup.close()
+        return True
+
+
+
+
+
+
+def main():
+    parser = create_parser()
+    args = parser.parse_args()
+    spracuj(args.subor[0], args.zapis)
 
 
 if __name__ == "__main__":
