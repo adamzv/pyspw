ab1741bcb48261104d0c51ce72f5cb6ca7bde192
Adam Zverka <adamzverka@hotmail.com>
2018-12-09 13:11:49 +0100

    Pridaný .travis.yml a ďalšie testy

--- tests/test_spracuj.py
+++ tests/test_spracuj.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import unittest
 from unittest import TestCase
 from pathlib import Path
 sys.path.append("../")
@@ -25,7 +26,27 @@ class Tests(TestCase):
         vysledok = spracuj(args.subor[0])
         self.assertTrue(vysledok)
 
+    def test_zapis_do_suboru_bez_w_argumentu(self):
+        with self.assertRaises(SystemExit):
+            args = self.parser.parse_args(["test.html", "-w"])
+
+    def test_zapis_do_suboru_s_argumentom(self):
+        args = self.parser.parse_args(["test.html", "-w", "test2.html"])
+        vysledok = spracuj(args.subor[0], args.zapis)
+        self.assertTrue(vysledok)
+
+    def test_zapis_do_rovnakeho_suboru(self):
+        #vytvorí ďalší súbor na testovanie
+        spracuj("test.html", "test2.html")
+
+        args = self.parser.parse_args(["test2.html", "-w", "test2.html"])
+        vysledok = spracuj(args.subor[0], args.zapis)
+        self.assertTrue(vysledok)
+
     def tearDown(self):
         subor = Path("test2.html")
         if subor.exists():
             os.remove("test2.html")
+
+if __name__ == "__main__":
+    unittest.main(buffer=False)
